3e9f946155d5f97650eccd6d9c8858a5ad4dc426
Nikolai Mavrenkov <koluch@koluch.ru>
2016-11-17 00:05:15 +0300

    Make static files copying on build and setup static files server

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,87 @@
 var webpack = require('webpack')
 var path = require('path')
 
-module.exports = [{
-    entry: {
-        app: "./src/js/app.js",
-        vendor: ["react", "react-dom"],
-    },
-    output: {
-        path: './bin/js',
-        filename: "[name].bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-            },
-            {
-                test: /\.scss$/,
-                loaders: [ 'style', 'css', 'sass' ],
-            }
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = [
+    // scripts
+    {
+        entry: {
+            app: "./src/js/app.js",
+            vendor: ["react", "react-dom"],
+        },
+        output: {
+            path: './bin/js',
+            filename: "[name].bundle.js"
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: 'babel',
+                },
+                {
+                    test: /\.scss$/,
+                    loaders: ['style', 'css', 'sass'],
+                }
+            ],
+        },
+        plugins: [
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor',
+                filename: 'vendor.bundle.js',
+            }),
+            //new webpack.DefinePlugin({
+            //    "process.env": {
+            //        NODE_ENV: JSON.stringify("production")
+            //    }
+            //})
         ],
+        resolve: {
+            alias: {'~': path.resolve(__dirname + '/src/js')},
+        }
     },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            filename: 'vendor.bundle.js',
-        }),
-        //new webpack.DefinePlugin({
-        //    "process.env": {
-        //        NODE_ENV: JSON.stringify("production")
-        //    }
-        //})
-    ],
-    resolve: {
-        alias: {'~': path.resolve(__dirname + '/src/js')},
+    // styles
+    {
+        entry: {
+            app: "./src/styles/app.scss",
+        },
+        output: {
+            path: './bin/css',
+            filename: "[name].bundle.css"
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.scss$/,
+                    loaders: ['style', 'css', 'sass'],
+                }
+            ],
+        },
+    },
+    // static
+    {
+        context: path.join(__dirname, '.'),
+        output: {
+            path: '',
+            filename: '[name]',
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.scss$/,
+                    loaders: ['style', 'css', 'sass'],
+                }
+            ],
+        },
+        plugins: [
+            new CopyWebpackPlugin([
+                {
+                    from: path.join(__dirname, '/src/static'),
+                    to: path.join(__dirname, '/bin')
+                },
+            ])
+        ]
     }
-}, {
-    entry: {
-        app: "./src/styles/app.scss",
-    },
-    output: {
-        path: './bin/css',
-        filename: "[name].bundle.css"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.scss$/,
-                loaders: [ 'style', 'css', 'sass' ],
-            }
-        ],
-    },
-}]
+]
